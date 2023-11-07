630fc911cb4796bf1f022f3bba2dcca64410dc4c
Agustin <82629916+agusder95@users.noreply.github.com>
2023-11-07 17:36:55 -0300

    Update colors.js

--- src/assets/styles/colors.js
+++ src/assets/styles/colors.js
@@ -4,7 +4,7 @@ export const colors = {
 	allWhite: '#ffffff',
 	secWhite:'#e7e6e7',
 	mainWhite: '#f4f1e9',
-	primary: '#232f3e',
+	primary: '#26323f',
 	secondary: '#37475a',
 	red: '#F45B69',
 	greenDark: '#36453B',
