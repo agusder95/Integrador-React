63fa80a5e47b274a32a757574a414ed29ca68ced
Agustin <agustin.derobles1995@gmail.com>
2024-08-22 20:50:23 -0300

    run build

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-/* import { AppWrapper } from "./AppStyle"; */
 import { Provider } from "react-redux";
 import { MenuContextProvider } from "./context/MenuHmb";
 import MyRoutes from "./routes/MyRoutes";
 import store from "./redux/store/store"
-/* import api from "./utils/api.js";
-
-async function fetchData() {
-  try{
-    const response = await api.get("/test");
-    console.log(response.data)
-  }catch(err){
-    console.log(err)
-  }
-}
-
-fetchData() */
-
 function App() {
   return (
     <div>
@@ -30,16 +16,3 @@ function App() {
 
 export default App;
 
-{
-  /* <nav>
-  <ul>
-    <li>
-      <a href="/">Home</a>
-    </li>
-    <li>
-      <a href="/Dummy">Dummy</a>
-    </li>
-  </ul>
-</nav>;
- */
-}
